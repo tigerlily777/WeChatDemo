0fbeda4a8a8c36062efe75c933db1bb150a6c7c2
Tiger HU <tigerlilyh777@gmail.com>
2025-04-10 01:00:28 +1200

    Build bottom bar icon and bottom bar

--- app/src/main/java/com/example/wechatdemo/MainActivity.kt
+++ app/src/main/java/com/example/wechatdemo/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.wechatdemo.ui.theme.WeChatDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +29,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeChatDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column {
+                    //WeChatBottomBar()
                 }
             }
         }
@@ -31,17 +38,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun WeChatBottomBar(selected: Int) {
+    Row {
+        TabItem(
+            if (selected == 0) R.drawable.ic_chat_filled else R.drawable.ic_chat_outlined,
+            "Chat",
+            Modifier.weight(1f)
+        )
+        TabItem(
+            if (selected == 1) R.drawable.ic_contacts_filled else R.drawable.ic_contacts_outlined,
+            "Contacts",
+            Modifier.weight(1f)
+        )
+        TabItem(
+            if (selected == 2) R.drawable.ic_discovery_filled else R.drawable.ic_discovery_outlined,
+            "Discover",
+            Modifier.weight(1f)
+        )
+        TabItem(
+            if (selected == 3) R.drawable.ic_me_filled else R.drawable.ic_me_outlined,
+            "Me",
+            Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+private fun TabItem(@DrawableRes iconId: Int, title: String, modifier: Modifier) {
+    Column(
+        modifier = Modifier.padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            painter = painterResource(iconId),
+            contentDescription = title,
+            modifier = Modifier.size(24.dp),
+        )
+        Text(text = title, fontSize = 11.sp)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WeChatDemoTheme {
-        Greeting("Android")
-    }
+fun TabItemPreview() {
+    TabItem(iconId = R.drawable.ic_chat_outlined, title = "Chat", modifier = Modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeChatBottomBarPreview() {
+    WeChatBottomBar(selected = 0)
 }
