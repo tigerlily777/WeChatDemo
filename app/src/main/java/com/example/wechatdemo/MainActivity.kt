20aad76eaf167b8deec8c65da553945a1b4ac60d
Tiger HU <tigerlilyh777@gmail.com>
2025-04-11 00:58:16 +1200

    Add pager

--- app/src/main/java/com/example/wechatdemo/MainActivity.kt
+++ app/src/main/java/com/example/wechatdemo/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.wechatdemo.ui.theme.WeChatDemoTheme
 
@@ -17,7 +22,21 @@ class MainActivity : ComponentActivity() {
             WeChatDemoTheme {
                 Column {
                     val viewModel: WeViewModel = viewModel()
-                    //WeChatBottomBar(onSelectedChange = )
+                    val pagerState = rememberPagerState(pageCount = {
+                        10
+                    })
+                    HorizontalPager(state = pagerState) { page ->
+                        when (page) {
+                            0 -> Box(Modifier.fillMaxSize())
+                            1 -> Box(Modifier.fillMaxSize())
+                            2 -> Box(Modifier.fillMaxSize())
+                            3 -> Box(Modifier.fillMaxSize())
+                        }
+
+                    }
+                    WeChatBottomBar(selectId = viewModel.selectedTab) {
+                        viewModel.selectedTab = it
+                    }
                 }
             }
         }
